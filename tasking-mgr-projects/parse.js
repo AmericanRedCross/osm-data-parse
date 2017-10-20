bfdf0712ee089887021e060df18f9081643af630
Matt <matthew.gibb@redcross.org>
2017-10-20 16:53:52 -0400

    csv parse updates

--- tasking-mgr-projects/parse.js
+++ tasking-mgr-projects/parse.js
@@ -13,7 +13,7 @@ var missingmaps = [];
 var tasksFc = { type: 'FeatureCollection', features: [] };
 
 var throttleProjects = function(cb){
-  var targetCount = 2500;
+  var targetCount = 4000;
   var counter = 0;
   for (var i=0;i<targetCount;i++) {
      (function(ind) {
@@ -31,24 +31,23 @@ var throttleProjects = function(cb){
 var fetchProjectData = function(projectNumber, cb) {
   request({
     method: 'GET',
-    uri: "http://tasks.hotosm.org/api/v1/project/" + projectNumber
+    uri: "http://tasks.hotosm.org/api/v1/project/" + projectNumber + "/summary"
   }, function (error, response, body) {
     if (!error && response.statusCode == 200) {
       var jsonResponse = JSON.parse(body);
-      if(jsonResponse.properties){
+      if(jsonResponse){
         /// capitalization or presence/lack of a space in Missing Maps shouldn't matter
-        var nameCheck = jsonResponse.properties.name.replace(/\s+/g, '').toLowerCase().indexOf("missingmaps");
+        var nameCheck = jsonResponse.name.replace(/\s+/g, '').toLowerCase().indexOf("missingmaps");
         if(nameCheck !== -1){
           // projectList.push(projectNumber); // # # # compile list of project numbers to next fetch detailed task data
           var projectObj = {
             "task_number": projectNumber,
-            "created" : jsonResponse.properties["created"].slice(0,10),
-            "name": jsonResponse.properties["name"].replace(/"/g,""),
-            "changeset_comment":jsonResponse.properties["changeset_comment"],
-            "author": jsonResponse.properties["author"],
-            "status":jsonResponse.properties["status"],
-            "done": jsonResponse.properties["done"],
-            "validated": jsonResponse.properties["validated"]
+            "created" : jsonResponse["created"].slice(0,10),
+            "name": jsonResponse["name"].replace(/"/g,""),
+            "author": jsonResponse["organisationTag"],
+            "status":jsonResponse["status"],
+            "done": jsonResponse["percentMapped"],
+            "validated": jsonResponse["percentValidated"]
           }
           missingmaps.push(projectObj);
           console.log("missingmaps :  " + projectNumber);
