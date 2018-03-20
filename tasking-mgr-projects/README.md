c1dfec172ca1eea4181ee8c751507b202f1c5e2b
danbjoseph <danbjoseph@gmail.com>
2018-03-20 15:50:10 -0400

    osm-data-parse shouldnt miss projects, prompts for parameterts

--- tasking-mgr-projects/README.md
+++ tasking-mgr-projects/README.md
@@ -1,12 +1,10 @@
 ### tasking-mgr-projects
 
-Simple node.js script that grabs basic details on all Missing Maps projects from the [HOT tasking manager](tasks.hotosm.org) (filters based on the project name) and saves out a CSV.
+Simple node.js script that grabs basic details on all projects from the [HOT tasking manager](tasks.hotosm.org) (filtered based on the searching the project name for a text string) and saves out as a CSV. Optionally also creates a GeoJSON of all task features marked as completed or validated).
 
-Also saves out a GeoJSON of all task features that are marked completed or validated.
-
-Currently setup for our specific use-case. But would be easy to configure.
-
-- download or clone the project
+- download or clone this project repository
+- install [`nvm`](https://github.com/creationix/nvm) to manage node versions on your system
+- run `nvm use` to read the '.nvmrc' file and switch to the correct version of node (8.10.0)
 - run `npm install` in the folder
-- update the line `var targetCount = 2500;` to a number greater than the most recent project
 - run `node parse.js`
+- provide run time parameters when prompted
--- tasking-mgr-projects/parse.js
+++ tasking-mgr-projects/parse.js
@@ -1,151 +1,170 @@
-var fs = require('fs');
-var path = require('path');
-var flow = require('flow');
+var Promise = require('bluebird');
+var inquirer = require('inquirer');
 var request = require('request');
 var converter = require('json-2-csv');
+var fs = require('fs');
 var turf = require('@turf/helpers');
-var moment = require("moment");
-
+var moment = require('moment');
+var path = require('path');
 
 var timestamp = moment().format('YYYYMMDD-HHmmss');
-var missingmaps = [];
-// var projectList = [];
-// # # # output with all mapped or validate project tiles
+
+var searchTerm, startProject, maxProject, creatGeo, projectsList;
+var matchCollection = [];
 var summaryFc = { type: 'FeatureCollection', features: [] };
 
-var startProject = 718;
-var maxProject = 730;
-
-var throttleProjects = function(cb){
-  var targetCount = maxProject - startProject;
-  var counter = 0;
-  for (var i=startProject;i<maxProject+1;i++) {
-     (function(ind) {
-        // # # # delay in milliseconds, should result in 2 second spacing between calls
-        var timeoutTime = (2000 * (ind - startProject)); 
-         setTimeout(function(){
-           // # # # throttle process to limit the speed of calls to download files from the server
-           fetchProjectData(ind, function(){
-             counter ++;
-             if(counter === targetCount){ cb(); }
-           })
-         }, timeoutTime);
-     })(i);
+var questions = [
+  {
+    type: 'input',
+    name: 'searchTerm',
+    message: 'Match projects based on what text string? (Project titles will have spaces removed and be lowercased.)',
+    default: 'missingmaps'
+  },
+  {
+    type: 'input',
+    name: 'startProject',
+    message: 'Start with what project number? (For "missingmaps" this can be 718.)',
+    validate: function(value) {
+      var pass = value.match(
+        /^[0-9]+$/
+      );
+      if (pass) {
+        return true;
+      }
+      return 'Please enter a valid number.';
+    }
+  },
+  {
+    type: 'input',
+    name: 'maxProject',
+    message: 'End with what project number?',
+    validate: function(value) {
+      var pass = value.match(
+        /^[0-9]+$/
+      );
+      if (pass) {
+        return true;
+      }
+      return 'Please enter a valid number.';
+    }
+  },
+  {
+    type: 'confirm',
+    name: 'createGeo',
+    message: 'Create GeoJSON output?',
+    default: true
   }
-}
+]
 
-var fetchProjectData = function(projectNumber, cb) {
-  request({
-    method: 'GET',
-    uri: "http://tasks.hotosm.org/api/v1/project/" + projectNumber + "/summary"
-  }, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      var jsonResponse = JSON.parse(body);
-      if(jsonResponse){
-        /// capitalization or presence/lack of a space in Missing Maps shouldn't matter
-        var nameCheck = jsonResponse.name.replace(/\s+/g, '').toLowerCase().indexOf("missingmaps");
-        if(nameCheck !== -1){
-          // projectList.push(projectNumber); // # # # compile list of project numbers to next fetch detailed task data
-          var projectObj = {
-            "task_number": projectNumber,
-            "created" : jsonResponse["created"].slice(0,10),
-            "name": jsonResponse["name"].replace(/"/g,""),
-            "author": jsonResponse["organisationTag"],
-            "status":jsonResponse["status"],
-            "done": jsonResponse["percentMapped"],
-            "validated": jsonResponse["percentValidated"]
+function fetchPrjData(projectNumber) {
+  return new Promise((resolve, reject) => {
+    request({ method: 'GET',
+      uri: "http://tasks.hotosm.org/api/v1/project/" + projectNumber + "/summary"
+    },(error, res, body) => {
+      if (!error && res.statusCode == 200){
+        var jsonResponse = JSON.parse(body);
+        if (jsonResponse) {
+            var nameCheck = jsonResponse.name.replace(/\s+/g, '').toLowerCase().indexOf(searchTerm);
+            if (nameCheck !== -1) {
+              var projectObj = {
+                "task_number": projectNumber,
+                "created" : jsonResponse["created"].slice(0,10),
+                "name": jsonResponse["name"].replace(/"/g,""),
+                "author": jsonResponse["organisationTag"],
+                "status":jsonResponse["status"],
+                "done": jsonResponse["percentMapped"],
+                "validated": jsonResponse["percentValidated"]
+              }
+              console.log("match for task :  " + projectNumber);
+              matchCollection.push(projectObj);
+              resolve(projectObj)
+          } else { 
+            console.log("no match for   :  " + projectNumber); 
+            resolve();
           }
-          missingmaps.push(projectObj);
-          console.log("missingmaps :  " + projectNumber);
-        } else { console.log("other task  :  " + projectNumber); }
-      }
-      cb();
-    } else {
-      console.log("failed     :  " + projectNumber)
-      console.log("error      :  " + error )
-      cb();
-    }
-  });
-}
-
-var throttleTasks = function(cb){
-  var targetCount = 0;
-  var counter = 0;
-  targetCount = missingmaps.length;
-  for (var i=0;i<targetCount;i++) {
-     (function(ind) {
-         setTimeout(function(){
-           // # # # throttle process to limit the speed of calls to download files from the server
-           fetchTaskData(ind, function(){
-             counter ++;
-             if(counter === targetCount){ cb(); }
-           });
-         }, 500 + (100 * ind));
-     })(i);
-  }
-}
-
-var fetchTaskData = function(prjIndex, cb) {
-  var thisPrj = missingmaps[prjIndex];
-  console.log("https://tasks.hotosm.org/api/v1/project/" + thisPrj["task_number"]);
-  // https://tasks.hotosm.org/api-docs/swagger-ui/index.html?url=https://tasks.hotosm.org/api/docs#/
-  request({
-    method: 'GET',
-    uri: "https://tasks.hotosm.org/api/v1/project/" + thisPrj["task_number"]
-  }, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      var jsonResponse = JSON.parse(body);
-      var prjFc = jsonResponse.tasks;
-      var prjTasks = prjFc.features;
-      for(var i=0; i<prjTasks.length; i++){
-        var tile = prjTasks[i];
-        var tileState = tile.properties.taskStatus;
-        if(tileState === "MAPPED" || tileState === "VALIDATED") {
-          var tileProp = {
-            "project": jsonResponse.projectId,
-            "task": tile.properties.taskId,
-            "state": tileState
-          };
-          summaryFc.features.push(turf.feature(tile.geometry, tileProp));
         }
-
+      } else {
+        console.log("failed fetchProjectData for :  " + projectNumber)
+        console.log(JSON.stringify(res) + '\n' )   
+        resolve();
       }
-      console.log("processed tasks for #" + thisPrj["task_number"]);
-      cb();
-    } else {
-      console.log("failed getting tasks json for #" + thisPrj["task_number"])
-      console.log("error      :  " + error )
-      cb();
-    }
+    });
+  })
+}
+
+var fetchTaskData = function(projectNumber) {
+  return new Promise((resolve,reject) => {
+    request({
+      method: 'GET',
+      uri: "https://tasks.hotosm.org/api/v1/project/" + projectNumber
+    },(error, res, body) => {
+      if (!error && res.statusCode == 200) {
+        var jsonResponse = JSON.parse(body);
+        var prjFc = jsonResponse.tasks;
+        var prjTasks = prjFc.features;
+        for (var i=0; i<prjTasks.length; i++) {
+          var tile = prjTasks[i];
+          var tileState = tile.properties.taskStatus;
+          if(tileState === "MAPPED" || tileState === "VALIDATED") {
+            var tileProp = {
+              "project": jsonResponse.projectId,
+              "task": tile.properties.taskId,
+              "state": tileState
+            };
+            summaryFc.features.push(turf.feature(tile.geometry, tileProp));
+          }
+        }
+        console.log("processed tasks for #" + projectNumber);
+        resolve();
+      } else {
+        console.log("failed getting tasks json for #" + projectNumber)
+        console.log("error      :  " + error )
+        resolve();
+      }
+    });
+  })  
+}
+
+function parseTasks() {
+  Promise.map(matchCollection, function(matchedPrj) {
+    return fetchTaskData(matchedPrj["task_number"]);
+  },{ concurrency: 1 }).then(function(resolved) {
+    var filePath = path.join(__dirname,"output", "output_" + timestamp + ".geojson");
+    fs.writeFile(filePath, JSON.stringify(summaryFc), (err) => {
+      if(err) throw err;
+      console.log("the GeoJSON has been written!")
+    });
   });
 }
 
-var parseTasks = flow.define(
-  function(){
-    throttleTasks(this);
-  },
-  function(){
-    var filePath = path.join(__dirname,"output", "output_" + timestamp + ".geojson");
-    fs.writeFile(filePath, JSON.stringify(summaryFc));
-  }
-);
-
-var parseProjects = flow.define(
-  function(){
-    throttleProjects(this);
-  },
-  function(){
+function parseProjects() {
+  Promise.map(projectsList, function(prjNumber) {
+    return fetchPrjData(prjNumber);
+  },{ concurrency: 1 }).then(function(resolved) {
     var options = {
       delimiter : {
-       wrap  : '"', // Double Quote (") character
-   }
+        wrap  : '"', // Double Quote (") character
+      }
     }
-    converter.json2csv(missingmaps, function(err, csv){
+    converter.json2csv(matchCollection, function(err, csv) {
       var filePath = path.join(__dirname,"output", "output_" + timestamp + ".csv");
-      fs.writeFile(filePath, csv);
-      parseTasks();
+      fs.writeFile(filePath, csv, (err) => {
+        if (err) throw err;
+        console.log("the CSV has been written!")
+      });
+      if (createGeo == true) { parseTasks(); }
     }, options);
-  }
-);
+  });
+}
 
-parseProjects();
+inquirer.prompt(questions).then(answers => {
+  startProject = parseInt(answers.startProject);
+  maxProject = parseInt(answers.maxProject);
+  createGeo = answers.createGeo;
+  searchTerm = answers.searchTerm;
+  projectsList = [];
+  for (var i = startProject; i < maxProject+1; ++i) {
+    projectsList.push(i);
+  }
+  parseProjects();
+});
